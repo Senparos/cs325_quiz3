a8325f798f1c455f6430e9fcf5ca7ac2bf191d44
Leonardo De La Torre Cruz <you@yourdomain.example.com>
2024-01-29 18:56:49 -0600

    VOO

--- finance.py
+++ finance.py
@@ -18,5 +18,7 @@ def monitor_stock_price(ticker, interval=10):
         time.sleep(interval)
 
 if __name__ == "__main__":
-    SYMBOL = 'VOO'  # Replace with the desired stock symbol
+
+   SYMBOL = 'VOO'  # Replace with the desired stock symbol
+
     monitor_stock_price(SYMBOL)
